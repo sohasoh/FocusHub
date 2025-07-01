c146afa25e32e59c54159c87c0efdf721c406b5f
sohasoh <soh.silas@giortech.com>
2025-07-01 19:35:54 +0200

    update

--- focus/src/App.js
+++ focus/src/App.js
@@ -2,30 +2,17 @@ import './App.css';
 
 function App() {
   return (
-     <div className='Login'>
-      <p>Sign-Up</p>
-      <div className='inpute'>
-      <div className='log'>
-        <input type='Name' name='Name' placeholder='Enter your name' required></input>
-      </div>
-      <div className='log'>
-        <input type='surename' name='Name' placeholder='Enter your Sure-name' required></input>
-      </div> 
-      <div className='log'>
-        <input type='Email' name='Name' placeholder='Enter your Email' required></input>
-      </div>
-      <div className='log'>
-        <input type='Password' name='Name' placeholder='Password' required></input>
-      </div>
-      <div className='log'>
-        <input type='password' name='Name' placeholder='Confirm Password' required></input>
-      </div>
-      <div className='valid'>
-        <a href='App.js'><button type='submit'>Submit</button></a>
-        
-      </div>
-      </div>
-     
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
