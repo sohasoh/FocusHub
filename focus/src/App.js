02c7c200f39e05c50fe0377883d5ffcb671fad4a
sohasoh <soh.silas@giortech.com>
2025-07-02 10:18:45 +0200

    update

--- focus/src/App.js
+++ focus/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
+import bluelogo from './bluelogo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Head">
+      <header className="Nav">
+        <img src={bluelogo} className="App-logo" alt="logo" />
+        <div className='connect'>
+          <link
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            <input type='button'>Create Account</input>
+          </link>
+          <link
+            className="login"
+            href="login.js"
+            target="_blank"
+            rel="noopener noreferrer">
+            <input type='button'>Login</input>
+          </link>
+        </div>
       </header>
     </div>
   );
