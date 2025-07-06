5887c6c82007215585b519f0feffed0111badf31
sohasoh <soh.silas@giortech.com>
2025-07-06 17:13:56 +0200

    update

--- focus/src/App.js
+++ focus/src/App.js
@@ -1,39 +1,21 @@
 import './App.css';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import Welcome from './Pages/welcome';
-import Home from './Pages/home';
 import Login from './login';
-import Creatacc from './Pages/creatacc';
-import Schedule from './Pages/schedule';
-import Team from './Pages/team';
-import Chat from './Pages/chat';
-import Aichat from './Pages/aichat';
-import {Route,createBrowserRouter,createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <React.Fragment>
-      <Route path="/welcome" element={<Welcome />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/creatacc" element={<Creatacc />} />
-      <Route path="/schedule" element={<Schedule />} />
-      <Route path="/team" element={<Team />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/aichat" element={<Aichat />} />
+      <Route path='/welcome' element={<Welcome />} />
+      <Route index element={<Login />} />
     </React.Fragment>
   )
 );
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
-
 
 const App = () => {
   return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
 
--- focus/src/login.js
+++ focus/src/login.js
@@ -2,13 +2,16 @@ import './login.css';
 function Login() {
   return ( 
 <div className='Login'>
-      <p>Sign-Up</p>
+      <p className='up'>Sign-Up</p>
       <div className='inpute'>
         <div className='log'>
           <input type='Name' name='Name' placeholder='Enter your name' required></input>
         </div>
         <div className='log'>
           <input type='surename' name='Name' placeholder='Enter your Sure-name' required></input>
+        </div> 
+         <div className='log'>
+          <input type='number' name='Name' placeholder='tel' required></input>
         </div> 
         <div className='log'>
           <input type='Email' name='Name' placeholder='Enter your Email' required></input>
@@ -20,7 +23,7 @@ function Login() {
           <input type='password' name='Name' placeholder='Confirm Password' required></input>
         </div>
         <div className='valid'>
-          <a href='App.js'><button type='submit'>Submit</button></a>
+          <a href='App.js'><input type='submit' className='submit'></input></a>
         </div>
       </div>
      
