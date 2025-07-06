9372211be806345cc322e7df29cdaaf3e070b7a1
sohasoh <soh.silas@giortech.com>
2025-07-06 17:58:32 +0200

    changes

--- focus/src/App.js
+++ focus/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import React from 'react';
 import Welcome from './Pages/welcome';
 import Login from './login';
+import Home from './Pages/home';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <React.Fragment>
       <Route path='/welcome' element={<Welcome />} />
-      <Route index element={<Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route index element={<Home />} />
     </React.Fragment>
   )
 );
---
+++ focus/src/Pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Pages/home.css'
+
+const home = () => {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
+
+export default home
