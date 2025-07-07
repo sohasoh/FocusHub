6eb944b79ab91b564190a95d8e3b0b920eaf3cd1
sohasoh <soh.silas@giortech.com>
2025-07-07 10:27:26 +0200

    update

--- focus/src/login.js
+++ focus/src/login.js
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom'
-import './login'
+import './login.css'
 const login = () => {
   return (
     <div className="login">
       <p className="top">Login</p>
-      <div className="inpute">
+      <div className="inputes">
         <div className="go">
           <input type="email" name="Email" placeholder="Enter your Email" required />
         </div>
         <div className="go">
           <input type="password" name="Password" placeholder="Password" required />
         </div>
+        <Link to="/create" className="forgot">Create Account</Link>
         <div className="valid">
-          <Link to="/home"></Link><input type="submit" className="submit" value="Login" />
+          <Link to="/home"></Link><input type="submit" className="yes" value="Login" />
         </div>
       </div>
     </div>
