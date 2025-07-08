280b442e2b02860b61c7f27f635bdb938d6488f4
sohasoh <soh.silas@giortech.com>
2025-07-08 09:23:05 +0200

    add

---
+++ focus/focus/src/Pages/home.js
@@ -0,0 +1,38 @@
+import './home.css'
+import bluelogo from '../bluelogo.svg';
+import { FaHome } from 'react-icons/fa';
+import { MdMessage } from "react-icons/md";
+import { RiTeamFill } from "react-icons/ri";
+import { AiFillWechat } from "react-icons/ai";
+import homeImg from '../home-img.png';
+import { IoMdMore } from "react-icons/io";
+import { RiCalendarScheduleFill } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
+const home = () => {
+  return (
+    <div>
+        <header className='head'>
+            <Link to="/"><img src={bluelogo} className="App-logo" alt="logo" /></Link>
+            <nav className='menue'>
+                <ul>
+                  <li><Link to="/home"><FaHome /> Home</Link></li>
+                  <li><Link to="/team"><RiTeamFill />Team</Link></li>
+                  <li><Link to="/chat"><MdMessage />Chat</Link></li>
+                  <li><Link to="/aichat"><AiFillWechat />AI Chat</Link></li>
+                  <li><Link to="/schedule"><RiCalendarScheduleFill />Schedule</Link></li>
+                </ul>
+            </nav>
+            <div className='logout'>
+                <Link to="/"><button className='logout-button'>Logout</button></Link>
+                <IoMdMore />
+            </div>
+        </header>
+        <section className='home-img'>
+          <img src={homeImg} alt='home-img' className='img1'/>
+        </section>
+    </div>
+  )
+}
+
+export default home
---
+++ focus/focus/src/login.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import './login.css'
+const login = () => {
+  return (
+    <div className="login">
+      <p className="top">Login</p>
+      <div className="inputes">
+        <div className="go">
+          <input type="email" name="Email" placeholder="Enter your Email" required />
+        </div>
+        <div className="go">
+          <input type="password" name="Password" placeholder="Password" required />
+        </div>
+        <Link to="/create" className="forgot">Create Account</Link>
+        <div className="valid">
+          <Link to="/home"><input type="submit" className="yes" value="Login" /></Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default login
