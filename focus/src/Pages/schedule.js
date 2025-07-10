081a7e3ebc4bf6a7f6d3ea505eb978d872602301
sohasoh <soh.silas@giortech.com>
2025-07-10 10:24:56 +0200

    add

--- focus/src/Pages/schedule.js
+++ focus/src/Pages/schedule.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import './schedule.css'
 import { Link } from 'react-router-dom';
+import { BsThreeDotsVertical } from "react-icons/bs";
 import { IoIosNotifications } from "react-icons/io";
 import bluelogo from '../bluelogo.svg';
 import Sidebar from '../Components/Sidebar' 
+
 
 const schedule = () => {
   return (
     <div className='sch'>
       <Sidebar />
-      <div>
+      <div className='calplan'>
         <div className='schnav'>
-          <Link to='/home'><img src={bluelogo} className="team-logo" alt="logo" /></Link>
-          <IoIosNotifications size='60px' />
+          <Link to='/home'><img src={bluelogo} className="sch-logo" alt="logo" /></Link>
+          <IoIosNotifications size='40px' />
+          <BsThreeDotsVertical size='30px'/>
         </div>
-        
+        <section className='Sched'>
+          <div className='plan'>
+
+          </div>
+          <div className='calenda'>
+            
+          </div>
+        </section>
       </div>
     </div>
   )
