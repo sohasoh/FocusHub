70037c9d25b72ef5082cff74bd00a8dd4ed56b79
sohasoh <soh.silas@giortech.com>
2025-07-10 14:38:07 +0200

    changes

--- src/Pages/chat.js
+++ src/Pages/chat.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './chat.css'
 import bluelogo from '../bluelogo.svg';
-import Messagebar from '../Components/messagebar'
+import Chatbar from '../Components/chatbar'
 import {Link} from 'react-router-dom';
 import Sidebar from '../Components/Sidebar' 
 
@@ -14,7 +14,7 @@ const chat = () => {
           <Link to='/home'><img src={bluelogo} className="team-logo" alt="logo" /></Link>
         </div>
         <section className='chatsection'>
-        <Messagebar/>
+        <Chatbar/>
         <div>
           heo
         </div>
