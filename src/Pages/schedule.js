45dd13ca635bf1fcbc82fe9c2db7e454ce3b5c48
sohasoh <soh.silas@giortech.com>
2025-07-10 18:51:50 +0200

    delete

--- src/Pages/schedule.js
+++ src/Pages/schedule.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import './schedule.css'
 import { Link } from 'react-router-dom';
-import { BsThreeDotsVertical } from "react-icons/bs";
-import { IoIosNotifications } from "react-icons/io";
 import bluelogo from '../bluelogo.svg';
 import Sidebar from '../Components/Sidebar' 
 
@@ -11,20 +9,23 @@ const schedule = () => {
   return (
     <div className='sch'>
       <Sidebar />
-      <div className='calplan'>
-        <div className='schnav'>
-          <Link to='/home'><img src={bluelogo} className="sch-logo" alt="logo" /></Link>
-          <IoIosNotifications size='40px' />
-          <BsThreeDotsVertical size='40px'/>
+      <div className='ho'>
+        <div className='calplan'>
+          <div className='schnav'>
+            <Link to='/home'><img src={bluelogo} className="sch-logo" alt="logo" /></Link>
+          </div>
+          <section className='Sched'>
+            <div className='plan'>
+              <div className='schedules'>
+                <div className='board1'></div>
+                <div className='board2'></div>
+              </div>
+            </div>
+            <div className='calenda'>
+              silas
+            </div>
+          </section>
         </div>
-        <section className='Sched'>
-          <div className='plan'>
-            silas
-          </div>
-          <div className='calenda'>
-            silas
-          </div>
-        </section>
       </div>
     </div>
   )
