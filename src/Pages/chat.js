0046916be7b5fd031ec3d4012802c6c769cc52f8
sohasoh <soh.silas@giortech.com>
2025-07-10 14:00:02 +0200

    change

--- src/Pages/chat.js
+++ src/Pages/chat.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './chat.css'
 import bluelogo from '../bluelogo.svg';
+import Messagebar from '../Components/messagebar'
 import {Link} from 'react-router-dom';
 import Sidebar from '../Components/Sidebar' 
 
@@ -8,8 +9,16 @@ const chat = () => {
   return (
     <div className='cha'>
       <Sidebar />
-      <div>
-        <Link to='/home'><img src={bluelogo} className="team-logo" alt="logo" /></Link>
+      <div className='hey'>
+        <div className='chathead'>
+          <Link to='/home'><img src={bluelogo} className="team-logo" alt="logo" /></Link>
+        </div>
+        <section className='chatsection'>
+        <Messagebar/>
+        <div>
+          heo
+        </div>
+      </section>
       </div>
     </div>
   )
